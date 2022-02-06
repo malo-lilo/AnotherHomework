2d7721ce10d7963e22af499d158d1c86628a6174
malo-lilo <98219124+malo-lilo@users.noreply.github.com>
2022-02-06 23:50:57 +0300

    Add files via upload
    Task № 8, about age.

---
+++ Lesson8Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*Запросить у пользователя год его рождения
+Если год < 1991, то вывести сообщение, что он рожден в СССР
+Если год >= 1991 вывести на экран каждый четный год до текущего*/
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите ваш год рождения");
+        int yearOfBirth = Convert.ToInt32(Console.ReadLine());
+        if (yearOfBirth < 1991)
+        {
+            Console.WriteLine("Вы рождены в СССР");
+        }
+        else
+        {
+            var date = DateTime.Now;
+            var dateY = date.Year;
+            int c = dateY % 2;
+            if (c > 0)
+            {
+                Console.WriteLine("В таком случае, четный год до текущего = " + (dateY - 1));
+            }
+            else
+            {
+                Console.WriteLine("В таком случае, четный год до текущего = " + (dateY - 2));
+            }
+        }
+    }
+}
